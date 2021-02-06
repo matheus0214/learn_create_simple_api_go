14b3bee5dab18b77906d3eac23af981fe243afff
matheus0214 <matheusgiga123@gmail.com>
2021-02-06 18:13:49 -0300

    refactor: :recycle: Add users controller
    Separete controller from routes users in specif folder

---
+++ modules/users/controllers/users_cotroller.go
@@ -0,0 +1,66 @@
+package controllers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/matheus0214/projects/learn_gin/modules/users/models"
+	"github.com/matheus0214/projects/learn_gin/modules/users/services"
+)
+
+// UsersController implemets routes to user
+type UsersController struct{}
+
+// Show route to get specif user
+func (ur *UsersController) Show(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"user": "Matheus",
+	})
+}
+
+// Create create one user
+func (ur *UsersController) Create(c *gin.Context) {
+	datas, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Erro to get datas",
+		})
+		return
+	}
+
+	newUser := &models.UserModel{}
+	err = json.Unmarshal(datas, newUser)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Erro to process user datas",
+		})
+		return
+	}
+
+	errorsUser := newUser.ValidateFieldsUser()
+	if errorsUser != nil {
+		c.JSON(http.StatusBadRequest, errorsUser)
+		return
+	}
+
+	_, err = services.CreateUser(newUser)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"error": models.ErrInvalidFieldEmailErrorUser.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, newUser)
+	return
+}
+
+// Options show options from user routes
+func (ur *UsersController) Options(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"METHODS ALLOWED": "GET, POST",
+		"Authorization":   "Bearer Token",
+	})
+}
